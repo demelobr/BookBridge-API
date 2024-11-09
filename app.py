3d040b4959082bb5bb8f6becb5b9a9f509d98b5c
Bruno Melo <demelobr10@gmail.com>
2024-11-09 18:14:09 -0300

    Adicionando a log_request_info para capturar informações sobre as requisições gerando um arquivo com logs(app.log).

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import logging
+from flask import request
 from sql_alchemy import db
 from app import create_app
 
 app = create_app()
+
+# Configuração básica de logs
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
+@app.before_request
+def log_request_info():
+    app.logger.info(f'Request URL: {request.url}')
+    app.logger.info(f'Request Method: {request.method}')
+    app.logger.info(f'Client IP: {request.remote_addr}')
+    app.logger.info(f'Request Headers: {request.headers}')
 
 if __name__ == '__main__':
     with app.app_context():
